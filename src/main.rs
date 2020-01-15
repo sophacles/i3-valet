5e5216cc2e74d78a50723ce841504bafdaa7b5d0
Erich Heine <sophacles@gmail.com>
2020-01-14 21:27:22 -0600

    Clean ups
    * Remove dead code
    * Simplify code where possible
    * Add help for args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate clap;
 extern crate env_logger;
 extern crate i3ipc;
@@ -11,10 +12,10 @@ use i3ipc::event::Event;
 use i3ipc::{I3Connection, I3EventListener, Subscription};
 
 use i3_valet::collapse::clean_current_workspace;
-use i3_valet::floats::teleport_float;
+use i3_valet::floats::{teleport_float, Loc, Positioning};
 use i3_valet::info;
 
-fn listenery_shit(command_conn: &mut I3Connection) {
+fn listener(command_conn: &mut I3Connection) {
     let mut listener = I3EventListener::connect().unwrap();
 
     let subs = [Subscription::Binding, Subscription::Window];
@@ -22,17 +23,9 @@ fn listenery_shit(command_conn: &mut I3Connection) {
 
     for evt in listener.listen() {
         match evt.unwrap() {
-            Event::WindowEvent(e) => {
-                debug!("Saw WindowEvent: {:#?}", e);
-            }
             Event::BindingEvent(e) => {
                 debug!("Saw BindingEvent: {:#?}", e);
-                let mut args: Vec<String> = e
-                    .binding
-                    .command
-                    .split_whitespace()
-                    .map(|s| String::from(s))
-                    .collect();
+                let mut args: Vec<&str> = e.binding.command.split_whitespace().collect();
                 if args.remove(0) == "nop" {
                     let cl = args.join(" ");
                     match make_args()
@@ -40,7 +33,7 @@ fn listenery_shit(command_conn: &mut I3Connection) {
                         .get_matches_from_safe(args)
                     {
                         Ok(m) => dispatch(m, command_conn),
-                        Err(e) => warn!("Cannot parse: {}\n (error was: {:?})", cl, e),
+                        Err(e) => warn!("Cannot parse: {} => {}", cl, e.message),
                     }
                 }
             }
@@ -53,32 +46,38 @@ fn make_args<'a, 'b>() -> App<'a, 'b> {
     App::new("i3-valet")
         .version("0.1")
         .author("sophacles@gmail.com")
-        .about("tools for i3 wm")
-        .subcommand(SubCommand::with_name("fix"))
+        .about("tend to your windows")
+        .subcommand(SubCommand::with_name("fix").about("clean up the window tree"))
         .subcommand(
             SubCommand::with_name("loc")
+            .about("Move a floating window to anchor point")
                 .arg(
                     Arg::with_name("how")
+                        .help("Positioning of window.\n'abs' is relative to the output\n'rel' is relative to the content area\n")
                         .index(1)
                         .required(true)
                         .possible_values(&["abs", "rel"]),
                 )
                 .arg(
                     Arg::with_name("where")
+                        .help("Anchor point to position window\n")
                         .index(2)
                         .required(true)
                         .possible_values(&["nw", "ne", "sw", "se", "bot", "top", "left", "right"]),
                 ),
         )
         .subcommand(
-            SubCommand::with_name("print").arg(
+            SubCommand::with_name("print")
+            .about("Print window information")
+            .arg(
                 Arg::with_name("target")
+                    .help("what to print")
                     .index(1)
                     .required(true)
                     .possible_values(&["tree", "rects", "window"]),
             ),
         )
-        .subcommand(SubCommand::with_name("listen"))
+        .subcommand(SubCommand::with_name("listen").about("connect to i3 socket and wait for events"))
 }
 
 // TODO: make this happy with all the options and stuff
@@ -91,8 +90,8 @@ fn dispatch(m: ArgMatches, conn: &mut I3Connection) {
             let m = m.subcommand.unwrap().matches;
             teleport_float(
                 conn,
-                m.value_of("where").unwrap().to_string().parse().unwrap(),
-                m.value_of("how").unwrap().to_string().parse().unwrap(),
+                value_t!(m.value_of("where"), Loc).expect("possible values broke!"),
+                value_t!(m.value_of("how"), Positioning).expect("possible_values broke!"),
             );
         }
         Some("print") => {
@@ -104,9 +103,9 @@ fn dispatch(m: ArgMatches, conn: &mut I3Connection) {
                 _ => unreachable!("stupid possible_values failed"),
             }
         }
-        Some("listen") => info!("Cannot dispatch listen: cli command only."),
+        Some("listen") => warn!("Cannot dispatch listen: cli command only."),
         None => info::print_ws(conn, &info::STD),
-        Some(f) => info!("Invalid command: {}", f),
+        Some(f) => warn!("Invalid command: {}", f),
     }
 }
 
@@ -114,12 +113,11 @@ fn main() {
     env_logger::init();
 
     let mut conn = I3Connection::connect().expect("i3connect");
-    //dispatch_old(args, &mut conn);
 
     let app = make_args();
     let parsed = app.get_matches();
     match parsed.subcommand_name() {
-        Some("listen") => listenery_shit(&mut conn),
+        Some("listen") => listener(&mut conn),
         _ => dispatch(parsed, &mut conn),
     }
 }
